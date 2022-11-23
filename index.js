8157eedd999d4d84c94cace90541d2983dd7a68c
Grace-aloo <graceaaloo@gmail.com>
2022-11-23 16:06:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 28;
+const num4 = 8;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random()*10)+1;
+const mod = num3 % num4;
+const max = Math.max( 1, 4, 5, 6, 9, 12, 20)
